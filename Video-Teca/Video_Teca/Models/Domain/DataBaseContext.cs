d60a38a44c22805de85560b88d858a9ca7f96ccd
Randall Vargas <randallvargas555@gmail.com>
2023-05-17 22:28:10 -0600

    Project

---
+++ Video-Teca/Video_Teca/Models/Domain/DataBaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Video_Teca.Models.DTO;
+
+namespace Video_Teca.Models.Domain
+{
+   
+        public class DatabaseContext : IdentityDbContext<ApplicationUser>
+        {
+            public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        }
+   
+}
---
+++ Video-Teca/Video_Teca/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Video_Teca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
