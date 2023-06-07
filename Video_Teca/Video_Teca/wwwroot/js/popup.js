6cf690771d75807d1067a14edec1c816b9cd1a66
Randall Vargas <randallvargas555@gmail.com>
2023-06-07 08:56:08 -0600

    Client

---
+++ Video_Teca/Video_Teca/wwwroot/js/popup.js
@@ -0,0 +1,14 @@
+﻿var btnpopup = document.getElementById('btn-popup'),
+    overlay = document.getElementById('overlay'),
+    popup = document.getElementById('popup'),
+    btncerrarpopup = document.getElementById('btn-cerrar-popup');
+
+btnpopup.addEventListener('click', function () {
+    overlay.classList.add('active');
+    popup.classList.add('active');
+   
+});
+btncerrarpopup.addEventListener('click', function () {
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+});
